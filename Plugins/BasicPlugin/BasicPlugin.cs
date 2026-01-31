7c9ebe3498e85226658dd2d34cde478976f15301
Flumor <mo.attila@proton.me>
2026-01-31 16:01:42 +0100

    Comment changes

--- Plugins/BasicPlugin/BasicPlugin.cs
+++ Plugins/BasicPlugin/BasicPlugin.cs
@@ -173,8 +173,3 @@ namespace DiscordBot.Plugins.BasicPlugin
         public Task CleanupAsync() => Task.CompletedTask;
     }
 }
-<<<<<<< HEAD
-
-// Test comment for change
-=======
->>>>>>> dcbe1580fba8005f244bee0d9645b3ef16051a48
